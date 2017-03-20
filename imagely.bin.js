f2aa2b8e9951c1a26d3b522dcd06633936981806
Choshun Snyder <csnyder@pandora.com>
2017-03-20 19:58:59 -0400

    Moving callbacks to bin.

--- imagely.bin.js
+++ imagely.bin.js
@@ -3,6 +3,7 @@
 require('babel-register');
 var Imagely = require('./imagely.es6').default;
 var yargs = require('yargs');
+var imageSize = require('image-size');
 var args = yargs.argv;
 
 var source = args._[0];
@@ -19,12 +20,32 @@ options.log = args.log || args.l;
 options.batch = args.batch;
 
 if (options.log) {
-	callback = function(error, dimensions) {
+	callback = function(error) {
+		var dimensions;
+		try {
+			dimensions = imageSize(destination);
+		}
+		catch (exception) {
+			dimensions = { width: null, height: null };
+		}
+
 		if (error) {
 			console.log(error);
 		}
 		else {
 			console.log(dimensions.width, dimensions.height);
+		}
+	};
+}
+else if (options.batch) {
+	callback = function() {
+		if (this.batchIndex < this.batchLength) {
+			let html = this.setWindowData(this.originalHtmlString, JSON.stringify(this.json[this.batchIndex]));
+			this.page.setContent(html);
+
+			let batchDestination = this.makeUniqueDestination(this.destination, this.batchIndex);
+			this.renderPage(batchDestination);
+			this.batchIndex++;
 		}
 	};
 }
