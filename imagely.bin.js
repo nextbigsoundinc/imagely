b7f4a7744d45e6391fca395b3e002ca7eaa8568b
Choshun Snyder <choshun.snyder@gmail.com>
2017-06-08 17:20:15 -0400

    Batching callback (#9)
    * Adding yarn, upgrading everything to latest except phantom.
    Phantom breaks at 2.x with error `UnhandledPromiseRejectionWarning: Unhandled promise rejection phantom 2`
    
    * Adding babel packages and config.
    
    * Update imagery.bin.js as entry point.
    Once you add babel, set the entrypoint with `main` in `packages.json`. This file is still es5, but once you add `require('babel-core/register’);`, every file that is required can be es6.
    These helped `https://github.com/JaxCavalera/Tagged-DBserver`, from `http://stackoverflow.com/questions/33624104/how-do-i-setup-babel-6-with-node-js-to-use-es6-in-my-server-side-code`.
    
    * Update imagely.js to es6.
    I changed zero logic, only syntax.
    
    * Batching callback to use uuid for image name.
    
    * Adding logs, taking out `json.length` check because the data is now clean.
    
    * `logfilepath` passed in as option.
    
    * PR feedback
    
    * Being more general with log.

--- imagely.bin.js
+++ imagely.bin.js
@@ -2,6 +2,7 @@
 
 var Imagely = require('./lib/imagely.js').default;
 var yargs = require('yargs');
+var path = require('path');
 var imageSize = require('image-size');
 var args = yargs.argv;
 
@@ -9,6 +10,11 @@ var source = args._[0];
 var destination = args._[1];
 var options = {};
 var callback;
+var fs = require('fs');
+var logs = {
+	failure: [],
+	success: []
+};
 
 options.width = args.width || args.w;
 options.height = args.height || args.h;
@@ -17,6 +23,39 @@ options.bg = args.bg || args.b;
 options.json = args.json || args.d;
 options.log = args.log || args.l;
 options.batch = args.batch;
+options.logFilepath = args.logFilepath;
+
+function addToBatchLogs(json) {
+	var log;
+	var dimensions;
+	var filename = json.filename;
+
+	try {
+		dimensions = imageSize(destination);
+	}
+	catch (exception) {
+		dimensions = { width: null, height: null };
+	}
+
+	log = dimensions;
+	log.filename = filename;
+
+	// Check if truthy; account for null, 0, or undefined.
+	if (log.width && log.height) {
+		logs.success.push(log);
+	}
+	else {
+		logs.failure.push(log);
+	}
+}
+
+function writeLogFile(logFilepath) {
+	fs.writeFile(logFilepath, JSON.stringify(logs, null, 4), function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	});
+}
 
 if (options.log) {
 	callback = function(error) {
@@ -39,13 +78,18 @@ if (options.log) {
 else if (options.batch) {
 	callback = function() {
 		if (this.jsonIndex < this.json.length) {
-			var name = this.destination.split('.').slice(0, -1).pop();
-			var uniqueName = this.destination.replace(name, name + this.jsonIndex);
-			var html = this.setWindowData(this.originalHtmlString, JSON.stringify(this.json[this.jsonIndex]));
+			var json = this.json[this.jsonIndex];
+			var name = path.basename(this.destination);
+			var html = this.setWindowData(this.originalHtmlString, JSON.stringify(json));
+			var filename = this.destination.replace(name, json.filename + '.gif');
 
 			this.page.setContent(html);
-			this.renderPage(uniqueName);
+			this.renderPage(filename);
 			this.jsonIndex++;
+
+			addToBatchLogs(json);
+		} else {
+			writeLogFile(options.logFilepath);
 		}
 	};
 }
